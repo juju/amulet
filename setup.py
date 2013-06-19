b217e1668deb2281df98dfa0af7d3484a38c867c
Marco Ceppi <marco@ceppi.net>
2013-06-19 18:37:41 -0400

    Better description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='amulet',
       version='0.0.1',
-      description='Tools to help with test writing',
+      description='Tools to help with writing Juju Charm Functional tests',
       author='Marco Ceppi',
       author_email='marco@ceppi.net',
       url="https://launchpad.net/amulet",
