e03a59f95f2defcba41ff8c57389892cf2de9bb5
Marco Ceppi <marco@ceppi.net>
2013-06-19 18:31:16 -0400

    Setup and distutils

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='amulet',
+      version='0.0.1',
+      description='Tools to help with test writing',
+      author='Marco Ceppi',
+      author_email='marco@ceppi.net',
+      url="https://launchpad.net/amulet",
+      packages=['amulet'],
+      scripts=['bin/amulet'],
+      )
