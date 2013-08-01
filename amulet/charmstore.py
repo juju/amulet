01aca3f20ebaea0f5093daa7620f73e73703c827
Marco Ceppi <marco@ceppi.net>
2013-08-01 18:07:14 -0400

    Finish charmstore implementation

--- amulet/charmstore.py
+++ amulet/charmstore.py
@@ -10,6 +10,7 @@ class CharmStore(object):
                         host, version)
         self.version = version
         self.http = urllib3.PoolManager()
+        self.cache = {}
 
     def search(self, **kwargs):
         allowed_keys = ['name', 'text', 'autocomplete', 'categories', 'owner',
@@ -19,7 +20,7 @@ class CharmStore(object):
         q = ''
         for key, value in kwargs.items():
             if key in allowed_keys:
-                q = '%s%s=%s' % ('&' if q else '', key, value)
+                q = '%s%s%s=%s' % (q, '&' if q else '', key, value)
 
         try:
             results = self.query('%s?%s' % ('charms', q))
@@ -28,27 +29,36 @@ class CharmStore(object):
 
         return results['result']
 
-    def charm(self, name, series='precise', owner=None, version=None):
+    def charm(self, name, series='precise', owner=None, version=None,
+              use_cache=True):
+        if name in self.cache and not version and use_cache:
+            return self.cache[name]
+
         if version:
-            q = '%s/%s/%s-version' % (owner if owner else '', series, name,
-                                      version)
+            q = 'charm/%s/%s/%s-%s' % (owner if owner else '', series, name,
+                                       version)
+            try:
+                results = self.query(q)
+            except Exception:
+                raise
+
+            # Don't cache version lookups
+            return results
         else:
             r = self.search(name=name, series=series)
             if len(r) > 0:
-                q = r[0]['charm']['id']
+                results = r[0]
             else:
                 return False
 
-        try:
-            results = self.query(q)
-        except Exception:
-            raise
+        self.cache[name] = results
+        return results
 
     def query(self, url):
         api_url = '%s%s' % (self.endpoint, url)
         r = self.http.request('GET', api_url)
 
         if r.data and (r.status >= 200 or r.status < 400):
-            return json.loads(r.data.decode('UTF-8'))
+            return json.loads(r.data.decode())
         else:
             raise Exception('No data returned')
