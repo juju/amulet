c90aa4db84cf2872298558b047f72dbf3a7071ef
Marco Ceppi <marco@ceppi.net>
2013-08-07 21:57:19 -0400

    Re-work hooks.py in to python instead of bash

--- charms/sentry/hooks/hooks.py
+++ charms/sentry/hooks/hooks.py
@@ -1,4 +1,58 @@
-#!/bin/bash
+#!/usr/bin/python
 
-data=`relation-get`
-juju-log "$data"
+import os
+import sys
+import json
+import glob
+import subprocess
+
+
+relation_dir = '/opt/relations'
+unit_number = os.environ['JUJU_REMOTE_UNIT'].split('/')[1]
+data_dir = os.path.join(relation_dir, os.environ['JUJU_RELATION'], unit_number)
+endpoint, relation = os.environ['JUJU_RELATION'].split('-', 1)
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+with open(os.path.join(data_dir, 'id'), 'w') as f:
+    f.write(os.environ['JUJU_RELATION_ID'])
+
+with open(os.path.join(data_dir, 'data'), 'w') as f:
+    try:
+        data = subprocess.check_output(['relation-get', '--format=json'])
+    except:
+        pass
+    else:
+        f.write(data)
+
+if endpoint == "requires":
+    otherend = "provides"
+else:
+    otherend = "requires"
+
+other_data_dir = os.path.join(relation_dir, "%s-%s" % (otherend, relation))
+subprocess.call(['juju-log', 'Running: %s %s' % (os.environ['JUJU_RELATION'],
+                                                 otherend)])
+
+if not os.path.exists(other_data_dir):
+    sys.exit(0)
+
+for unit_data_dir in glob.glob(os.path.join(other_data_dir, '*')):
+    unit = os.path.basename(unit_data_dir)
+    with open(os.path.join(unit_data_dir, 'id')) as f:
+        relation_id = f.read().strip()
+
+    with open(os.path.join(unit_data_dir, 'data')) as f:
+        relation_data = json.loads(f.read().strip())
+
+    subprocess.call(['juju-log', json.dumps(relation_data)])
+    relation_cmd_line = ['relation-set', '-r', relation_id]
+    for key, val in relation_data.items():
+        if val is None:
+            relation_cmd_line.append('{}='.format(key))
+        else:
+            relation_cmd_line.append('{}={}'.format(key, val))
+
+    subprocess.call(['juju-log', ' '.join(relation_cmd_line)])
+    subprocess.check_call(relation_cmd_line)
