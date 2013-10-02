976d1ba4d2375d1dac305ddf9a2b00b3a43eaf81
Marco Ceppi <marco@ceppi.net>
2013-10-02 12:44:06 -0400

    Fixed PEP8

--- amulet/__init__.py
+++ amulet/__init__.py
@@ -4,5 +4,5 @@ from .waiter import wait
 
 from .deployer import Deployment
 from .charmstore import CharmStore
-from .helpers import TimeoutError, timeout, default_environment, raise_status,
-                     SKIP, PASS, FAIL
+from .helpers import (
+    TimeoutError, timeout, default_environment, raise_status, SKIP, PASS, FAIL)
