43cf20678d094e36b86d2789c0bcae9059e75943
Marco Ceppi <marco@ceppi.net>
2014-03-11 00:32:25 -0400

    More explicit testing format

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,7 +4,11 @@ import unittest
 import sys
 import yaml
 
-from amulet import helpers
+from amulet.helpers import (
+    JujuVersion,
+    environments,
+    default_environment,
+)
 
 from mock import patch, Mock
 
@@ -33,7 +37,7 @@ class HelpersTest(unittest.TestCase):
     @patch('subprocess.check_output')
     def test_jujuversion_go(self, mock_check_output):
         mock_check_output.side_effect = ['1.2.3-series-xxx']
-        version = helpers.JujuVersion()
+        version = JujuVersion()
 
         self.assertEqual(version.major, 1)
         self.assertEqual(version.minor, 2)
@@ -45,7 +49,7 @@ class HelpersTest(unittest.TestCase):
     @patch('subprocess.check_output')
     def test_jujuversion_py(self, mcheck_output):
         mcheck_output.side_effect = [Exception('Non-zero exit'), 'juju 8.6']
-        version = helpers.JujuVersion()
+        version = JujuVersion()
 
         self.assertEqual(version.major, 8)
         self.assertEqual(version.minor, 6)
@@ -57,7 +61,7 @@ class HelpersTest(unittest.TestCase):
     @patch('subprocess.check_output')
     def test_jujuversion_malformed(self, mcheck_output):
         mcheck_output.return_value = '1.2.3.45'
-        version = helpers.JujuVersion()
+        version = JujuVersion()
 
         self.assertEqual(version.major, 1)
         self.assertEqual(version.minor, 2)
@@ -74,22 +78,22 @@ class HelpersTest(unittest.TestCase):
         mock_open.return_value.read.return_value = RAW_ENVIRONMENTS_YAML
         mock_exists.return_value = True
 
-        envs = helpers.environments()
+        envs = environments()
         self.assertEqual(yaml.safe_load(RAW_ENVIRONMENTS_YAML), envs)
 
     @patch('os.path.isfile')
     def test_environments_noenv(self, mock_exists):
         mock_exists.return_value = False
 
-        self.assertRaises(IOError, helpers.environments)
+        self.assertRaises(IOError, environments)
 
-    @patch.object(helpers, 'environments')
+    @patch('amulet.helpers.environments')
     def test_default_environment(self, menvironments):
         menvironments.return_value = yaml.safe_load(RAW_ENVIRONMENTS_YAML)
-        default = helpers.default_environment()
+        default = default_environment()
         self.assertEqual('gojuju', default)
 
-    @patch.object(helpers, 'environments')
+    @patch('amulet.helpers.environments')
     def test_default_environment_no_default(self, menvironments):
         environments_yaml = """
         environments:
@@ -101,12 +105,12 @@ class HelpersTest(unittest.TestCase):
             admin-secret: zyxyx
             default-series: world"""
         menvironments.return_value = yaml.safe_load(environments_yaml)
-        self.assertEqual('gojuju1', helpers.default_environment())
+        self.assertEqual('gojuju1', default_environment())
 
-    @patch.object(helpers, 'environments')
+    @patch('amulet.helpers.environments')
     def test_default_environment_no_default_multi_fail(self, menvironments):
         envs = yaml.safe_load(RAW_ENVIRONMENTS_YAML)
         del envs['default']
         menvironments.return_value = envs
 
-        self.assertRaises(ValueError, helpers.default_environment)
+        self.assertRaises(ValueError, default_environment)
