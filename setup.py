7f107bf42e40b013b6f4d9a705e1740ee4cbb523
Marco Ceppi <marco@ceppi.net>
2015-10-23 18:16:35 -0400

    version bump

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ tests_require = [
 
 setup(
     name='amulet',
-    version='1.12.0',
+    version='1.12.1',
     description='Tools to help with writing Juju Charm Functional tests',
     install_requires=install_requires,
     package_data={'amulet': ['unit-scripts/amulet/*']},
     author='Marco Ceppi',
     author_email='marco@ceppi.net',
-    url="https://launchpad.net/amulet",
+    url="https://github.com/juju/amulet",
     packages=['amulet'],
 )
